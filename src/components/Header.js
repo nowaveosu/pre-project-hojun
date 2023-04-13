7f962778eded1983d2d1233b7c69bcd36495a7f3
nowaveosu <nowaveosu@gmail.com>
2023-04-14 00:28:21 +0900

    feat: scope done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 import largeLogo from "../static/large-logo.png"
 import smallLogo from "../static/small-logo.png"
+
 const Header = () => {
     const HeaderWrapper = styled.div`
         display: flex;
         align-items: center;
         justify-content: center;
-        background-color: whitesmoke;
-        color: #fff;
+        background-color: white;
+        color: #F8F9F9;
         height: 50px;
         border-top : 3px solid orange;
+        box-shadow : 0 0 3px gray;
 
     `;
     const Logo = styled.img`
@@ -24,24 +26,40 @@ const Header = () => {
             }
             &.smallLogo{
                 width: 35px;
+                padding-left : 20px;
                 display : block;
             }
         }
     `;
     const Menu = styled.a`
-        font-size: 15px;
+        font-size: 13px;
         padding-right: 22px;
         color : black;
         white-space: nowrap;
         text-decoration-line: none;
+        @media screen and (max-width: 800px) {
+            &.about, &.forteams {
+                display : none;
+            }
+        }
     
     `;
+    const Scope = styled.div`
+        display : none;
+        padding-right : 9px;
+        @media screen and (max-width: 600px) {
+            display : block;
+        }
+    `
     const Input = styled.input`
         width : 300px;
         height : 30px;
         margin-right : 5px;
         border-radius : 3px;
         border : 1px solid gray;
+        @media screen and (max-width: 600px) {
+            display : none;
+        }
     `
     const Button = styled.button`
         height : 34px;
@@ -67,10 +85,11 @@ const Header = () => {
         <HeaderWrapper>
             <Logo className='largeLogo' src={largeLogo} />
             <Logo className='smallLogo' src={smallLogo} />
-            <Menu href="About.js">About</Menu>
-            <Menu href="Products.js">Products</Menu>
-            <Menu href="ForTeams">For Teams</Menu>
+            <Menu className='about' href="About.js">About</Menu>
+            <Menu className='products' href="Products.js">Products</Menu>
+            <Menu className='forteams' href="ForTeams">For Teams</Menu>
             <Input placeholder='🔍 Search...'></Input>
+            <Scope className='scope'>🔍</Scope>
             <Button className='login'>Log in</Button>
             <Button className='signup'>Sign up</Button>
         </HeaderWrapper>
